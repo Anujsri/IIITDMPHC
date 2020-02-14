0f81d0602b2d31f9a30feb69f70ca3464e14222e
Anuj Srivastava <anuj96sri@gmail.com>
2020-02-14 12:14:06 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 module.exports={
-    mongoURI:'mongodb://anujsom:anuj123@ds245661.mlab.com:45661/talentgo',
-    googleClientID:'397294475789-t6tf7r6ajb0m4vde6la6fugde2snaguo.apps.googleusercontent.com',
-    googleClientSecret:'d6z8o3vC8UeLs6QuRR7B2Lyv'
+    mongoURI:'',
+    googleClientID:'',
+    googleClientSecret:''
 }
 
